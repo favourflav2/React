133de823f1dbb6da74bea76901273750ec55891f
favourflav <chukwagon1@gmail.com>
2022-11-18 13:39:31 -0800

    added

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const contain = document.getElementById('root');
+
+ReactDOM.render(<h1>favoursdgfsfg</h1>,contain);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+  module:{
+    rules:[
+        {
+            test: /\.js/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }
+    ]
+  }
+};
